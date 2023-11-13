e012045569142e7ce1e373a0ab0dbeba4377e5eb
Usuario <Usuario@192.168.2.102>
2023-11-13 11:02:07 -0600

    Mi aporte es un Hello World!

---
+++ src/main/java/com/mycompany/github/Github.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.github;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Github {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
