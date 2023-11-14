18bd2389a9c0416f3c3c3020cb18e2c246a2863c
Usuario <Usuario@192.168.2.102>
2023-11-13 18:10:50 -0600

    Hice una modificacion

--- src/main/java/com/mycompany/github/Github.java
+++ src/main/java/com/mycompany/github/Github.java
@@ -11,6 +11,6 @@ package com.mycompany.github;
 public class Github {
 
     public static void main(String[] args) {
-        System.out.println("HOLA A TODOS!");
+        System.out.println("esto es una modificacion!");
     }
 }
