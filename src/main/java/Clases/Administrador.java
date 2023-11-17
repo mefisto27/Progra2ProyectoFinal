26533d1c2034a6335c5c41d452ae545337e8b3b6
Usuario <Usuario@192.168.2.102>
2023-11-16 22:41:40 -0600

    Se crearon todos sus Atributos, Getters and Setters. Se crearon el constructor vacio y el construtor normal. Se no se creo el to String pero se puso una nota

--- src/main/java/Clases/Administrador.java
+++ src/main/java/Clases/Administrador.java
@@ -9,5 +9,23 @@ package Clases;
  * @author Usuario
  */
 public class Administrador extends Usuario{
-    
+    private String tipoAdministrador;
+
+    public String getTipoAdministrador() {
+        return tipoAdministrador;
+    }
+
+    public void setTipoAdministrador(String tipoAdministrador) {
+        this.tipoAdministrador = tipoAdministrador;
+    }
+
+    public Administrador() {
+    }
+
+    public Administrador(String tipoAdministrador, String nombre, String apellidos, String cedula, Direccion direccion, String correo, int numeroTelefonico) {
+        super(nombre, apellidos, cedula, direccion, correo, numeroTelefonico);
+        this.tipoAdministrador = tipoAdministrador;
+    }
+     /**Todavia no se va a añadir el to String hasta estar seguros 
+     * que no se van a agregar más atributos a la clase Cliente*/
 }
