c9749b4bd9f88bda8fb4be2ecc5bec1e20e7b64d
Usuario <Usuario@192.168.2.102>
2023-11-21 23:52:23 -0600

    Se crearon los metodos Editar/Agregar/Eliminar Usuarios

--- src/main/java/Clases/Administrador.java
+++ src/main/java/Clases/Administrador.java
@@ -37,33 +37,90 @@ public class Administrador extends Usuario {
         this.tipoAdministrador = tipoAdministrador;
     }
 
-    public void Agregar() {
+   public void Agregar(List<Usuario> clientes) {
+    Scanner scanner = new Scanner(System.in);
 
-        Scanner scanner = new Scanner(System.in);
+    System.out.print("Ingrese el nombre del nuevo usuario: ");
+    String nombre = scanner.nextLine();
+    System.out.print("Ingrese los apellidos del nuevo usuario: ");
+    String apellidos = scanner.nextLine();
+    System.out.print("Ingrese la cédula del nuevo usuario: ");
+    String cedula = scanner.nextLine();
+    System.out.println("Ingrese la direccion del nuevo usuario:");
+    Direccion direccion = new Direccion();
+    direccion.AgregarDireccion();
+    System.out.println("Ingrese el correo del nuevo usuario:");
+    String correo = scanner.nextLine();
+    System.out.println("Ingrese el numero del nuevo usuario:");
+    int numero = scanner.nextInt();
+    Usuario nuevoUsuario = new Usuario(nombre, apellidos, cedula, direccion, correo, numero);
 
-        System.out.print("Ingrese el nombre del nuevo usuario: ");
-        String nombre = scanner.nextLine();
-        System.out.print("Ingrese los apellidos del nuevo usuario: ");
-        String apellidos = scanner.nextLine();
-        System.out.print("Ingrese la cédula del nuevo usuario: ");
-        String cedula = scanner.nextLine();
-        System.out.println("Ingrese la direccion del nuevo usuario:");
-        Direccion direccion = new Direccion();
-        direccion.AgregarDireccion();
-        System.out.println("Ingrese el correo del nuevo usuario:");
-        String correo = scanner.nextLine();
-        System.out.println("Ingrese el numero del nuevo usuario:");
-        int numero = scanner.nextInt();
-        Usuario nuevoUsuario = new Usuario(nombre, apellidos, cedula, direccion, correo, numero);
+    // Agregar el nuevo usuario a la lista de clientes 
+    clientes.add(nuevoUsuario);
+    System.out.println("Usuario agregado correctamente.");
+}
 
-        // Agregar el nuevo usuario a la lista de clientes 
-        List<Usuario> clientes = new ArrayList<>();
-        clientes.add(nuevoUsuario);
-        System.out.println("Usuario agregado correctamente.");
+public void Editar(List<Usuario> clientes) {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Ingrese la cédula del usuario a editar:");
+    String cedula = scanner.nextLine();
+
+    for (Usuario usuario : clientes) {
+        if (usuario.getCedula().equals(cedula)) {
+            System.out.println("Usuario encontrado. Ingrese los nuevos datos:");
+
+            System.out.print("Nuevo nombre: ");
+            String nuevoNombre = scanner.nextLine();
+            usuario.setNombre(nuevoNombre);
+
+            System.out.print("Nuevos apellidos: ");
+            String nuevosApellidos = scanner.nextLine();
+            usuario.setApellidos(nuevosApellidos);
+
+            System.out.print("Nueva dirección:");
+            Direccion nuevaDireccion = new Direccion();
+            nuevaDireccion.AgregarDireccion();
+            usuario.setDireccion(nuevaDireccion);
+
+            System.out.print("Nuevo correo: ");
+            String nuevoCorreo = scanner.nextLine();
+            usuario.setCorreo(nuevoCorreo);
+
+            System.out.print("Nuevo número telefónico: ");
+            int nuevoNumero = scanner.nextInt();
+            usuario.setNumeroTelefonico(nuevoNumero);
+
+            System.out.println("Usuario editado correctamente.");
+            return;
+        }
     }
 
+    System.out.println("No se encontró un usuario con la cédula proporcionada.");
+}
+
+    public void Eliminar(List<Usuario> clientes) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese la cédula de la persona a eliminar");
+        String cedula = scanner.nextLine();
+        for (int i = 0; i < clientes.size(); i++) {
+            Usuario usuario = clientes.get(i);
+
+            if (usuario.getCedula().equals(cedula)) {
+                clientes.remove(i);
+                System.out.println("Usuario eliminado correctamente.");
+                return;
+            }
+        }
+        System.out.println("No se encontró un usuario con la cédula proporcionada.");
+    }
+  public  void imprimirLista(List<Usuario> clientes) {
+        for (Usuario usuario : clientes) {
+            System.out.println(usuario.getNombre() + " " + usuario.getApellidos() + " - " + usuario.getCedula());
+        }
     /**
      * Todavia no se va a añadir el to String hasta estar seguros que no se van
      * a agregar más atributos a la clase Cliente
      */
+}
 }
