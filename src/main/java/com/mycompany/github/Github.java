76fac56cc55edbe849543988210fd842343daa92
Usuario <Usuario@192.168.2.102>
2023-11-27 20:42:53 -0600

    Se creo una ecuacion en el Main

--- src/main/java/com/mycompany/github/Github.java
+++ src/main/java/com/mycompany/github/Github.java
@@ -11,6 +11,11 @@ package com.mycompany.github;
 public class Github {
 
     public static void main(String[] args) {
+        System.out.println("");
         System.out.println("esto es una modificacion!");
+        int numero1 = 5;
+                int numero2 = 5;
+                int resultado;
+                resultado = numero1 + numero2;
     }
 }
