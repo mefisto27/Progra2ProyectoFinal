6e65a43c86d70c3d59d0e631e326da8b2d27aacb
Usuario <Usuario@192.168.2.102>
2023-12-17 08:28:14 -0600

    Se realizo todo el backend de Productos, solo falta modificar el metodo actualizar

---
+++ src/main/java/Negocio/ProductosNegocio.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Negocio;
+
+import AccesoDatos.ProductosData;
+import AccesoDatos.iProductosData;
+import Clases.Excepciones.ProductoExcepcion;
+import Clases.Producto;
+import java.util.List;
+
+/**
+ *
+ * @author Usuario
+ */
+public class ProductosNegocio {
+
+    private final iProductosData productosData;
+
+    public ProductosNegocio() {
+        productosData = new ProductosData();
+    }
+
+    public List<Producto> ConsultarProductos() throws ProductoExcepcion, Exception {
+        return productosData.ConsultarProductos();
+    }
+
+    public Producto Consultar(String idProducto) throws ProductoExcepcion, Exception {
+        return productosData.Consultar(idProducto);
+    }
+    /*
+    public String Insertar(Producto producto) throws ProductoExcepcion, Exception {
+
+    }
+
+    public boolean Actualizar(Producto producto) throws ProductoExcepcion, Exception {
+
+    }
+
+    public void Eliminar(String idProducto) throws ProductoExcepcion, Exception {
+
+    } */
+}
--- src/main/java/Negocio/UsuariosNegocio.java
+++ src/main/java/Negocio/UsuariosNegocio.java
@@ -14,25 +14,30 @@ import java.util.List;
  * @author Usuario
  */
 public class UsuariosNegocio {
+
     private final iUsuariosData usuariosData;
 
     public UsuariosNegocio() {
-        usuariosData = new UsuarioData();
+        usuariosData = new UsuariosData();
     }
 
-     public List<Usuario> ConsultarUsuarios() throws UsuarioExcepcion, Exception {
+    public List<Usuario> ConsultarUsuarios() throws UsuarioExcepcion, Exception {
         return usuariosData.ConsultarUsuarios();
     }
+
     public String Insertar(Usuario usuario) throws UsuarioExcepcion, Exception {
-       return usuariosData.Insertar(usuario);
+        return usuariosData.Insertar(usuario);
     }
-     public Boolean Actualizar(Usuario usuario) throws UsuarioExcepcion, Exception {
-       return usuariosData.Actualizar(usuario);
+
+    public Boolean Actualizar(Usuario usuario) throws UsuarioExcepcion, Exception {
+        return usuariosData.Actualizar(usuario);
     }
-      public void Eliminar(String usuario) throws UsuarioExcepcion, Exception {
-       usuariosData.Eliminar(usuario);
-    } 
-      public boolean Login(Usuario usuario) throws UsuarioExcepcion, Exception{
-          return usuariosData.Login(usuario);
-      }
+
+    public void Eliminar(String cedula) throws UsuarioExcepcion, Exception {
+        usuariosData.Eliminar(cedula);
+    }
+
+    public boolean Login(Usuario usuario) throws UsuarioExcepcion, Exception {
+        return usuariosData.Login(usuario);
+    }
 }
